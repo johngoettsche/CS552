4ef49669b05e9c22c2802b58a25b17733340bc0f
John Goettsche <goet0969@vandals.uidaho.edu>
2015-02-06 16:36:24 -0800

    update 2/6/2015

--- hw1.c
+++ hw1.c
@@ -7,32 +7,53 @@
 #include <inttypes.h>
 #include <avr/io.h>
 
-void delay (unsigned int dly){
-	int i;
-	for(i = dly; i != 0; i--);
+uint8_t notOn = 0xff;
+uint8_t isOn = 0x00;
+unsigned int second = 65000U;
+unsigned int dly = 5000U;
+
+void delay (unsigned int thisDly){
+	volatile int i;
+	for(i = thisDly; i != 0; i--);
+}
+
+int monitor (){
+	uint8_t count = 0;
+	PORTB = isOn;  /* to show that a button has been pressed */
+	while(PIND != notOn){
+		count++;
+		delay(dly);
+	}
+	PORTB = notOn; /* turn off LEDs */
+	return count;
+}
+
+void display(unsigned int count){
+	volatile int i;
+	PORTB = isOn; /* display */
+	for(i = count; i != 0; i--){
+		asm volatile(	"nop\n\t"
+				"nop\n\t"
+				"nop\n\t"
+				"nop\n\t"
+				);
+		delay(dly);
+	}
+	PORTB = notOn; /* turn off LEDs */
 }
 
 int main(void){
-	uint8_t count = 0;
-	uint8_t notOn = 0xff;
-	uint8_t isOn = 0x00;
-	unsigned int dly = 1000U;
-	unsigned int adjust = 0U;
-	uint8_t i;
+	uint8_t pressTime = 0;
 	DDRB = 0xff;  /* enable port B for output */
 	DDRD = 0x00;  /* enable port D for input */
+	PORTB = isOn; /* to show that the system is on */
+	delay(second);
+	PORTB = notOn; /* turn off LEDs */
 	while(1){
-		if(PIND != notOn){
-			while(PIND != notOn){
-				count++;
-				delay(dly);
-			}
-			delay(65000U);
-			PORTB = isOn;
-			for(i = count; i != 0; i--){
-				delay(dly + adjust);
-			}
-			PORTB = notOn;
+		if(PIND != notOn){ /* button is pressed */
+			pressTime = monitor();
+			delay(second);
+			display(pressTime);
 		}
 	}
 	return(0);
